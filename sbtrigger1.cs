bc5734512a169c65baeaa197df6beac71bd7f321
Matt <macavall@microsoft.com>
2025-09-30 15:31:05 -0500

    first

---
+++ sbtrigger1.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace proj5;
+
+public class sbtrigger1
+{
+    private readonly ILogger<sbtrigger1> _logger;
+
+    public sbtrigger1(ILogger<sbtrigger1> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function(nameof(sbtrigger1))]
+    public async Task Run(
+        [ServiceBusTrigger("myqueue5", Connection = "sbconnstring", IsBatched = false, IsSessionsEnabled = true, AutoCompleteMessages = false)]
+        ServiceBusReceivedMessage message,
+        ServiceBusMessageActions messageActions)
+    {
+        _logger.LogInformation("Message ID: {id}", message.MessageId);
+        await messageActions.CompleteMessageAsync(message);
+
+        //foreach (var message in messages)
+        //{
+        //    _logger.LogInformation("Message ID: {id}", message.MessageId);
+        //    await messageActions.CompleteMessageAsync(message);
+        //}
+    }
+}
